0b5d3d0d1c0b1d1060d4022243a10dd1a032467f
Dmytro Strelnikov <d.strelnikov@inventati.org>
2020-07-15 15:05:24 +0200

    Test gradient calculation with different quadrature_degree
     How to reproduce the experiment
    -------------------------------
    
    1. Set up the desired implicitness parameter by editing
    implicitness.assign(Constant("1.0"))
    The recommended values are 1.0 and 0.5.
    
    2. Adjust the labels according to the chosen implicitness
    labels.append('impl 1.0, qd none')
    labels.append('impl 1.0, qd ' + str(deg))
    
    3. Adjust or remove the output file parameter
    outfile=args.scratch+'/gradient_test_plot.png'
    
    4. Run the simulation:
    python3 -i run.py

--- run.py
+++ run.py
@@ -12,7 +12,22 @@ time_space = np.linspace(0, T, num=Nt, endpoint=True)
 
 control = np.vectorize(u)(time_space, t1=0.005, t2=0.010)
 
-epsilons, deltas_fwd = gradient_test(
-    control, n=15, diff_type='forward', eps_init=2**-10)
-gradient_test_plot(epsilons, deltas_fwd)
+deltas = []
+labels = []
 
+implicitness.assign(Constant("1.0"))
+eps, delta = gradient_test(
+    control, n=12, diff_type='forward', eps_init=2**-12)
+
+deltas.append(delta)
+labels.append('impl 1.0, qd none')
+
+for deg in range(1,6):
+    parameters["form_compiler"]["quadrature_degree"] = deg
+    eps, delta = gradient_test(
+        control, n=12, diff_type='forward', eps_init=2**-12)
+    deltas.append(delta)
+    labels.append('impl 1.0, qd ' + str(deg))
+
+gradient_test_plot(eps, *deltas, labels=labels,
+    outfile=args.scratch+'/gradient_test_plot.png', dpi=160)
