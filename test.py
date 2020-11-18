27a41e8ab9a9d81f5d8524e26446c19ae5ef71dc
Dmytro Strelnikov <d.strelnikov@inventati.org>
2020-11-18 13:43:16 +0100

    Provide tests for hermine_interpolating_polynomial

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-import numpy as np
 import unittest
+
+import numpy as np
+# from numpy.polynomial import Polynomial
 
 import splines
 
@@ -18,6 +20,7 @@ class SplineTestCase(unittest.TestCase):
         s11 = splines.HermiteSpline([0, 1], [0, 0], [0, 1])
         self.assertTrue((s11.coef_array[1] == splines.h11).all())
 
+
     def test_values_on_unit_interval(self):
         # must reproduce x^3 + 2x^2 + 3x - 1
         s = splines.HermiteSpline([0, 1], [-1, 5], [3, 10])
@@ -27,6 +30,7 @@ class SplineTestCase(unittest.TestCase):
 
         for x in np.linspace(0, 1, 20, endpoint=True):
             self.assertAlmostEqual(s(x), p(x))
+
 
     def test_values(self):
         # must reproduce x^3 + 2x^2 + 3x - 1
@@ -38,6 +42,7 @@ class SplineTestCase(unittest.TestCase):
         for x in np.linspace(-1, 1, 20, endpoint=True):
             self.assertAlmostEqual(s(x), p(x))
 
+
     def test_linear_extrapolation(self):
         # must reproduce x^3 + 2x^2 + 3x - 1
         s = splines.HermiteSpline([-1, 1], [-3, 5], [2, 10],
@@ -46,3 +51,18 @@ class SplineTestCase(unittest.TestCase):
 
         self.assertTrue((s.coef_array[0] == [-1, 2, 0, 0]).all())
         self.assertTrue((s.coef_array[-1] == [-5, 10, 0, 0]).all())
+
+
+    def test_hermine_interpolating_polynomial(self):
+
+        p = splines.Polynomial(2 * np.random.rand(4) - 1)
+        knots = 2 * np.random.rand(2) - 1
+        values = p(knots)
+        derivatives = p.deriv()(knots)
+
+        p_interp = splines.hermine_interpolating_polynomial(
+                knots, values, derivatives)
+
+        self.assertTrue((p.domain == p_interp.domain).all())
+        self.assertTrue((p.window == p_interp.window).all())
+        self.assertTrue(np.allclose(p.coef, p_interp.coef))
