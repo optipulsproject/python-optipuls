1049084c90c5cd635a731885122925d719eaf1b3
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-02-01 12:09:00 +0100

    Setup experiment for numapde-talk-2
    Usage:
    python run.py --scratch /scratch/OptiPuls/numapde-talk-2
    
    Expected output:
    
    numapde-talk-2
    ├── gradient_test.png
    ├── linear_rampdown_0.000
    │   ├── paraview
    │   └── report.txt
    ├── linear_rampdown_0.005
    │   ├── paraview
    │   └── report.txt
    ├── linear_rampdown_0.010
    │   ├── paraview
    │   └── report.txt
    ├── linear_rampdown_0.015
    │   ├── paraview
    │   └── report.txt
    └── optimized
        ├── optimal_control.npy
        ├── optimal_control.png
        ├── paraview
        └── report.txt

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import argparse
+import pathlib
 
 import dolfin
 from dolfin import Constant
@@ -26,10 +27,10 @@ dolfin.set_log_level(40)
 dolfin.parameters["form_compiler"]["quadrature_degree"] = 1
 
 
-T, Nt = 0.015, 30
+T, Nt = 0.024, 240
 dt = T/Nt
 
-P_YAG = 2000.
+P_YAG = 1600.
 absorb = 0.135
 R_laser = 0.0002
 laser_pd = (absorb * P_YAG) / (np.pi * R_laser**2)
@@ -55,11 +56,12 @@ problem.solidus = 858.0
 problem.control_ref = np.zeros(Nt)
 problem.beta_control = 10**2
 problem.beta_velocity = 10**18
-problem.velocity_max = 0.15
+problem.velocity_max = 0.08
 problem.beta_liquidity = 10**12
 problem.beta_welding = 10**-2
-problem.threshold_temp = 1000.
-problem.target_point = dolfin.Point(0, .7*Z)
+# problem.threshold_temp = 1050.  # for 100 time steps
+problem.threshold_temp = 1095.  # for 200+ time steps?
+problem.target_point = dolfin.Point(0, .75*Z)
 problem.pow_ = 20
 
 # initialize FEM spaces
@@ -79,25 +81,62 @@ problem.kappa = coefficients.kappa
 
 time_space = np.linspace(0, T, num=Nt, endpoint=True)
 
+# running forward simulation with linear-rampdown pulse shape
+for rampdown_time in [0., 0.005, 0.010, 0.015]:
+    control = laser.linear_rampdown(time_space, 0.005, 0.005 + rampdown_time)
+    simulation = Simulation(problem, control)
+    simulation.name = 'linear_rampdown_' + f'{rampdown_time:1.3f}'
+
+    path = args.scratch+'/linear_rampdown_'+ f'{rampdown_time:1.3f}'
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+
+    with open(path + '/report.txt', 'w+') as file:
+        file.write(simulation.report)
+
+    io.save_as_pvd(
+        simulation.evo, simulation.problem.V, path + '/paraview/evo.pvd')
+    io.save_as_pvd(
+        simulation.evo_vel, simulation.problem.V1,
+        path + '/paraview/evo_vel.pvd')
+
+
 print('Creating a test simulation.')
 test_control = 0.5 + 0.1 * np.sin(0.5 * time_space / np.pi)
 test_simulation = Simulation(problem, test_control)
 
+vis.size_inches = (6, 4)
 epsilons, deltas_fwd = optimization.gradient_test(
         test_simulation, eps_init=10**-5, iter_max=15)
 vis.gradient_test_plot(
-        epsilons, deltas_fwd, outfile=args.scratch+'/gradient_test.png')
+        epsilons, deltas_fwd, outfile=args.scratch+'/gradient_test.pdf', transparent=True)
 print(f'Gradient test complete. See {args.scratch}/gradient_test.png')
 
-print('Creating an initial simulation.')
+print('Creating an initial guess simulation.')
 control = np.zeros(Nt)
 simulation = Simulation(problem, control)
 
 descent = optimization.gradient_descent(
-        simulation, iter_max=100, step_init=2**-25)
+        simulation, iter_max=25, step_init=2**-25)
 
-vis.control_plot(
-        descent[-1].control,
-        labels=['Optimal Control'],
-        outfile=args.scratch+'/optimal_control.png')
+optimal = descent[-1]
+
+path_optimized = args.scratch + '/optimized'
+pathlib.Path(path_optimized).mkdir(parents=True, exist_ok=True)
+with open(path_optimized + '/report.txt', 'w') as file:
+        file.write(optimal.report)
+
+io.save_as_pvd(
+        optimal.evo, optimal.problem.V,
+        path_optimized + '/paraview/evo.pvd')
+io.save_as_pvd(
+        optimal.evo_vel, optimal.problem.V1,
+        path_optimized + '/paraview/evo_vel.pvd')
+
+vis.optimal_control_plot(
+        optimal.control,
+        outfile=path_optimized+'/optimal_control.png')
+
+np.save(path_optimized+'/optimal_control.npy', optimal.control)
+
 print(f'Gradient descent complete. See {args.scratch}/optimal_control.png')
