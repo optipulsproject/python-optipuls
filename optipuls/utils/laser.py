8958112c15b2d7b8e9443cc74d39a31e2d6d31f5
Dmytro Strelnikov <d.strelnikov@inventati.org>
2022-01-27 14:59:08 +0100

    Add pulse exporting tool

--- optipuls/utils/laser.py
+++ optipuls/utils/laser.py
@@ -10,3 +10,16 @@ def linear_rampdown(t, t1=0.005, t2=0.010):
         return (t2-t)/(t2-t1)
     else:
         return 0.
+
+def pulse(control_abs, timeline):
+    '''Laser pulse as numpy array.
+
+    Parameters:
+        control_abs (array-like (Nt,), float):
+            The control function in absolute power scale (typically control * P_YAG).
+        timeline (array-like (Nt,), float):
+            Discrete time domain.
+
+    '''
+
+    return np.column_stack((control_abs, timeline))
